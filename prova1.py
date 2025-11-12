1de24cf5fed10e94f4708eb7d3491ecc74d18fef
mohamam <mmamouni21200@iesjoanramis.org>
2025-11-12 13:26:10 +0100

    ex1

--- prova1.py
+++ prova1.py
@@ -2,7 +2,7 @@ a= float(input("Escriu el primer nombre:"))
 b= float(input("Escriu el segon nombre:"))
 c= float(input("Escriu el tercer nombre:"))
 
-d=a+b*c
-print("El resultat de la suma {} + {} és {}".format(a, b, c, d))
+d=a+b+c
+print("El resultat de la suma {} + {} + {} es {}".format(a, b, c, d))
 d=a*b*c
-print("El resultat de la multiplicació {} * {} és {}".format(a, b, c, d))
+print("El resultat de la multiplicació {} * {} * {} es {}".format(a, b, c, d))
