2cbe89e89d9e70c09b6641110603076671121081
mohamam <mmamouni21200@iesjoanramis.org>
2025-11-18 09:08:19 +0100

    13.a

--- prova2.py
+++ prova2.py
@@ -1,2 +1,16 @@
 for i in range(10,-1,-1):
     print("{} * 5={}".format(i,i*5))
+"""
+
+a = int(input("Escriu el primer operand"))
+b = int(input("Escriu el segon operand"))
+if opcio==1:
+    print("La suma de {} + {} és".format(a, b, a+b))
+if opcio==2:
+    print("La resta de {} - {} és".format(a, b, a-b))
+if opcio==3:
+    print("La multiplicació de {} * {} és".format(a, b, a*b))
+if opcio==4:
+    print("La Divisió de {} / {} és".format(a, b, a/b))
+if opcio==0:
+    print("Sortir")
