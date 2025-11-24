33966defceac26553789837a305d44cf08df9cf6
mohamam <mmamouni21200@iesjoanramis.org>
2025-11-24 09:48:31 +0100

    a

--- Ex17.py
+++ Ex17.py
@@ -1,8 +1,14 @@
-"""Definir una funció que calculi la longitud d’una llista o d’una cadena donada. Prova-la amb diferents exemples.
+"""
+Definir una funció que calculi la longitud d’una llista
+ o d’una cadena donada. Prova-la amb diferents exemples.
 """
 def ex17(a):
     return len(a)
 
 # Programa principal
-a=[1, 3, 4, 5]
+a=[1, 3, 4, "Pere", [[3,4],5]]
 print(ex17(a))
+b="Som en Joan Ramis, i tú què tal estàs?"
+print(ex17(b))
+c=[]
+print(ex17(c))
---
+++ Ex18.py
@@ -0,0 +1,4 @@
+"""
+Definir una funció que agafi un caràcter i 
+retorni vertader si és una vocal i en cas contrari retorni fals. Prova-la amb diferents exemples.
+"""
