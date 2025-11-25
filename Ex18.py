fd5a9faec7b130c3a7d8dfbb0b2c3e5ff917ca59
mohamam <mmamouni21200@iesjoanramis.org>
2025-11-25 09:23:43 +0100

    a

--- Ex18.py
+++ Ex18.py
@@ -2,3 +2,14 @@
 Definir una funció que agafi un caràcter i 
 retorni vertader si és una vocal i en cas contrari retorni fals. Prova-la amb diferents exemples.
 """
+
+def ej18(c):
+    v = "aeiouAEIOUáàèéìíòóúùÀÁÈÉÌÍÒÓÙÚ"
+    if c in v:
+        return True
+    else:
+        return False
+    
+# Programa principal
+c = input("Escriu un caràcter per a provar si és o no vocal:")
+print(ej18(c))   
