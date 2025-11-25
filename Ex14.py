8ad640737dac5341ff3fff44d297a1294c2f20c1
mohamam <mmamouni21200@iesjoanramis.org>
2025-11-25 09:38:12 +0100

    a

--- Ex14.py
+++ Ex14.py
@@ -1,102 +1,103 @@
 def menu_principal():
-    opcio=0
-    while opcio<1 or opcio>3: 
-        opcio=int(input("""Elegeix una opció:
-                    1. Calculadora decimal
-                    2. Calculadora real (floats)
-                    3. Sortir \n"""))
-        if opcio>0 and opcio<4:
-             return opcio
+    while True:
+        opcio = int(input("""Elegeixi una opció:
+                            1. Calculadora decimal
+                            2. Calculadora real (floats)
+                            3. Canvi de base (binària, octal, decimal, hexadecimal)
+                            4. Sortir\n"""))
+        if opcio in [1, 2, 3, 4]:
+            return opcio
         else:
-            print("L'opcio seleccionada no és correcte, torn-ho a provar!!\n")
+            print("L'opció seleccionada no és correcta, torni-ho a provar!!\n")
+
 def menu_calculadora():
-    opcio=0
-    while opcio<1 or opcio>5:
-        opcio=int(input("""Elegeixi una opció:
-                1. Suma
-                2. Resta
-                3. Multiplicació
-                4. Divisió
-                5. Sortir 
-                """))
-        if opcio>0 and opcio<6:
-            return opcio 
+    while True:
+        opcio = int(input("""Elegeixi una opció:
+                            1. Suma
+                            2. Resta
+                            3. Multiplicació
+                            4. Divisió
+                            5. Sortir\n"""))
+        if opcio in [1, 2, 3, 4, 5]:
+            return opcio
         else:
-            print("Gràcies per utilitzar la meva calculadora, fins un altre dia!\n")
-def calculadora_decimal(opcio):
-    if opcio>0 and opcio<5:
-        a= int(input("Introdueix el primer numero:"))
-        b= int(input("Introdueix el segon numero:"))
-    match(opcio):
-        case 1:
-            #Suma
-            print("Estic fent la suma!\n")
-            c= a+b
-            print("El resultat de suma {} + {}és {}".format(a, b, c))
-        case 2:
-            #Resta
-            print("Estic fent la resta!\n")
-            c= a-b
-            print("El resultat de resta {} - {}és {}".format(a, b, c))
-        case 3:
-            #multiplicacio
-            print("Estic fent la multiplicació!\n")
-            c= a*b
-            print("El resultat de multiplicació {} * {}és {}".format(a, b, c))
-        case 4:
-            #Divisió
-            print("Estic fent la divisió!\n")
-            c= a//b
-            print("El resultat de divisió {} // {}és {}".format(a, b, c))
-        case _:
-            print("Opció no vàlida! Torni_ho a provar!\n")        
-def calculadora_real(opcio):
-    if opcio>0 and opcio<5:
-        a= float(input("Introdueix el primer numero:"))
-        b= float(input("Introdueix el segon numero:"))
-    match(opcio):
-        case 1:
-            #Suma
-            print("Estic fent la suma!\n")
-            c= a+b
-            print("El resultat de suma {} + {}és {}".format(a, b, c))
-        case 2:
-            #Resta
-            print("Estic fent la resta!\n")
-            c= a-b
-            print("El resultat de resta {} - {}és {}".format(a, b, c))
-        case 3:
-            #multiplicacio
-            print("Estic fent la multiplicació!\n")
-            c= a*b
-            print("El resultat de multiplicació {} * {}és {}".format(a, b, c))
-        case 4:
-            #Divisió
-            print("Estic fent la divisió!\n")
-            c= a/b
-            print("El resultat de divisió {} / {}és {}".format(a, b, c))
-        case _:
-            print("Opció no vàlida! Torni_ho a provar!\n")        
-def canvis_de_base
-print("Estic passant a binari!\n")
+            print("L'opció seleccionada no és correcta, torni-ho a provar!!\n")
+
+def calcular(opcio, tipus):
+    a = float(input("Introdueixi el primer número: "))
+    b = float(input("Introdueixi el segon número: "))
+
+    if tipus == "decimal":
+        operacions = {
+            1: (a + b, "suma"),
+            2: (a - b, "resta"),
+            3: (a * b, "multiplicació"),
+            4: (a / b if b != 0 else "Error: Divisió per zero!", "divisió")
+        }
+    else:  # real
+        operacions = {
+            1: (a + b, "suma"),
+            2: (a - b, "resta"),
+            3: (a * b, "multiplicació"),
+            4: (a / b if b != 0 else "Error: Divisió per zero!", "divisió")
+        }
+
+    resultat, operacio = operacions.get(opcio, ("Opció no vàlida!", ""))
+    print(f"Estic fent la {operacio}! \nEl resultat de {operacio} {a} i {b} és {resultat}")
+
+# Funció per a canviar entre bases
+def canviar_base():
+    print("Elegeixi una opció de base:")
+    print("1. Binari a Decimal")
+    print("2. Octal a Decimal")
+    print("3. Hexadecimal a Decimal")
+    print("4. Decimal a Binari")
+    print("5. Decimal a Octal")
+    print("6. Decimal a Hexadecimal")
+    opcio = int(input("Selecciona una opció (1-6): "))
+
+    if opcio == 1:
+        num = input("Introdueix el número binari: ")
+        try:
+            print(f"{num} en binari és {int(num, 2)} en decimal.")
+        except ValueError:
+            print("Error: El número introduït no és binari.")
+    elif opcio == 2:
+        num = input("Introdueix el número octal: ")
+        try:
+            print(f"{num} en octal és {int(num, 8)} en decimal.")
+        except ValueError:
+            print("Error: El número introduït no és octal.")
+    elif opcio == 3:
+        num = input("Introdueix el número hexadecimal: ")
+        try:
+            print(f"{num} en hexadecimal és {int(num, 16)} en decimal.")
+        except ValueError:
+            print("Error: El número introduït no és hexadecimal.")
+    elif opcio == 4:
+        num = int(input("Introdueix el número decimal: "))
+        print(f"{num} en decimal és {bin(num)[2:]} en binari.")
+    elif opcio == 5:
+        num = int(input("Introdueix el número decimal: "))
+        print(f"{num} en decimal és {oct(num)[2:]} en octal.")
+    elif opcio == 6:
+        num = int(input("Introdueix el número decimal: "))
+        print(f"{num} en decimal és {hex(num)[2:]} en hexadecimal.")
+    else:
+        print("Opció no vàlida!")
 
 # Programa Principal
-
-op=1
-while op!=0:
-    op=menu_principal()
-    if op==1:
-        # Calculadora decimal
-        print("Estic passant per la calculadora decimal!\n")
-        calculadora_decimal(menu_calculadora())
-    elif op==2:
-        # Calculadora real
-        print("Estic passant per la calculadora real!\n")
-        calculadora_real(menu_calculadora())
-    elif op==3:
-        #canvis de base 
-        print("Estic passant a binari!\n")
-        canvis_de_base
+while True:
+    op = menu_principal()
+    if op == 1:
+        print("Calculadora decimal activada!")
+        calcular(menu_calculadora(), "decimal")
+    elif op == 2:
+        print("Calculadora real activada!")
+        calcular(menu_calculadora(), "real")
+    elif op == 3:
+        print("Canvi de base activat!")
+        canviar_base()
     else:
-        print("Gràcies per a utilitzar la meva calculadora, fins un altre dia!\n")
-        op=0
+        print("Gràcies per utilitzar la calculadora. Fins un altre dia!")
+        break
