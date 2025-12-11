b9c65419a404faf14b02f6056cb478f4359ae2b3
mohamam <mmamouni21200@iesjoanramis.org>
2025-12-11 08:42:45 +0100

    a

---
+++ Python classe/Ex23.py
@@ -0,0 +1,6 @@
+"""Definir una funció crear_repetits() que agafi un número enter i un caràcter i retorni el caràcter multiplicat pel número enter.
+ Ex: crear_repetits(5, “a”), retorni “aaaaa”"""
+
+def crear_repetits(num, char):
+    return char * num
+print(crear_repetits(5, "a"))  # Output: aaaaa
