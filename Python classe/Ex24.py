c79f2640e4b0cfc4a7ceb1cc844e54c542da9764
mohamam <mmamouni21200@iesjoanramis.org>
2025-12-11 08:46:26 +0100

    A

---
+++ Python classe/Ex24.py
@@ -0,0 +1,8 @@
+"""Definir una funció crear_punts() que agafi una llista de números i que pinti per 
+pantalla tants punts com el valor de cada número de la llista. Entre els elements de la llista, hi ha d’haver 
+un salt de línia. Ex: crear_punts([2,3,4]) mostri per pantalla el següent:
+"""
+def crear_punts(llista):
+    for num in llista:
+        print('.' * num) 
+crear_punts([2, 3, 4]) 
